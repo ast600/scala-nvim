e2b56f08f4b98a1d5efcadc4d23ba0c7490ac364
Vasilii Sotnikov <vsotnikov@icloud.com>
2025-04-09 23:28:30 +0300

    Fixed Telescope ref, added Git integration and turned on line numbering

--- init.lua
+++ init.lua
@@ -1,16 +1,19 @@
 package.path = "./?.lua;" .. package.path
 
+vim.opt.number = true
+
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
-Plug('scalameta/nvim-metals', { ['tag'] = '0.10.x', ['for'] = { 'scala', 'sbt', 'java' } })
+Plug('scalameta/nvim-metals', { ['tag'] = 'v0.10.x', ['for'] = { 'scala', 'sbt', 'java' } })
 Plug('j-hui/fidget.nvim', { ['tag'] = 'v1.6.1' })
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/cmp-buffer')
 Plug('hrsh7th/nvim-cmp')
+Plug('lewis6991/gitsigns.nvim', { ['tag'] = 'v1.0.2' })
 Plug('navarasu/onedark.nvim')
 
 vim.call('plug#end')
@@ -19,3 +22,4 @@ vim.cmd('silent! colorscheme onedark')
 require('mytelescope').setup_bindings()
 require('mycmp').setup()
 require('mymetals').setup()
+require('gitsigns').setup()
