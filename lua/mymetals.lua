af2c80b4168b64de41d0a67445b37957ebf71dfd
Vasilii Sotnikov <vsotnikov@icloud.com>
2025-05-01 20:46:30 +0300

    added buffer diagnostics, hovering

--- lua/mymetals.lua
+++ lua/mymetals.lua
@@ -32,6 +32,8 @@ function module:setup()
 	vim.keymap.set("n", "gr", vim.lsp.buf.references)
 	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+  vim.keymap.set("n", "<leader>bd", vim.diagnostic.setloclist)
+  vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
 end
 
 return module
