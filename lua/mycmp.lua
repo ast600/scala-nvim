2324e5acdb1a40d364d407b59b3c7d31b74bb39b
Vasilii Sotnikov <vsotnikov@icloud.com>
2025-09-24 21:26:36 +0300

    Autocompletion tweaks

--- lua/mycmp.lua
+++ lua/mycmp.lua
@@ -6,8 +6,9 @@ function module:setup()
         sources = {{name = 'nvim_lsp'}, {name = 'buffer'}},
         snippet = {expand = function(args) vim.snippet.expand(args.body) end},
         mapping = cmp.mapping.preset.insert({
-            ['<CR>'] = cmp.mapping.confirm({select = true})
-        })
+            ['<CR>'] = cmp.mapping.confirm({select = false})
+        }),
+        completion = {autocomplete = false}
     })
 end
 
